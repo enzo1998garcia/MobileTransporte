dc6c7522b2698fa5edd4bdda051e49a1569fd7fc
enzo <enzo1998garcia@gmail.com>
2023-06-27 21:45:58 -0300

    Creacion Del Proyecto

---
+++ screens/Buscador.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Buscador() {
+  return (
+    <View>
+      <Text>Buscador</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ screens/Transporte.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Transporte() {
+  return (
+    <View>
+      <Text>Transporte</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
